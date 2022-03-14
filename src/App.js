bbbc5067df4b88689f2cea93e40c400118571be8
Ryan Mwakio <ryanmwakio6@gmail.com>
2022-03-14 14:33:01 +0300

    initial congiguration

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Helmet } from "react-helmet";
+import logo from "./assets/icons/logo.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className=" ">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <link rel="shortcut icon" href={logo} type="image/x-icon" />
+          <title>Ryan M | Software Developer</title>
+          <link rel="canonical" href="http://mysite.com/example" />
+        </Helmet>
+        <div class=" text-primarypink">r</div>
+      </div>
+    </>
   );
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      transparent: "transparent",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      red: colors.red,
+      //custom colors start here
+      blue: colors.blue,
+      primarypurple: "#8826D1",
+      primarypink: "#BD0F71",
+      lightpurple: "#9F09FC",
+      lightpink: "#E30057",
+      primaryorange: "#F8550E",
+      primarygreen: "#2FF80E",
+      primarycharcoalblack: "#232427",
+      primaryblack: "#1B1C20",
+      primarygrey: "#79797A",
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
