15ad609d1fdd2b9158a92d2cf8ff40cd24bd6572
Ryan Mwakio <ryanmwakio6@gmail.com>
2022-03-15 21:07:32 +0300

    footer complete

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,12 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Error.css";
 
 function Error() {
   return (
     <>
       <div className="error-page">
-        <div>
-          <h4>sorry the page you're looking for cannot be found</h4>
+        <div className="error-graphic">
+          <h4>404!</h4>
+          <Link to="/" className="back-link">
+            Go to home page{" "}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6 text-primarypurple inline-block"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M14 5l7 7m0 0l-7 7m7-7H3"
+              />
+            </svg>
+          </Link>
         </div>
       </div>
     </>
